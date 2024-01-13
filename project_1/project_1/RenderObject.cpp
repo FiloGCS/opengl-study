87e6dba21d2d9b4a15095e8904e471bc793ff6c7
Guillermo Couceiro Salido <guillermo.couceiro@gmail.com>
2024-01-13 07:06:31 +0100

    more preparing

--- project_1/project_1/RenderObject.cpp
+++ project_1/project_1/RenderObject.cpp
@@ -3,11 +3,10 @@ constexpr char DEFAULT_SHADER[] = "07_usingProjection";
 #include "RenderObject.h"
 
 RenderObject::RenderObject() : shader(DEFAULT_SHADER){
-
+	//VAO
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
-
-	// Mesh Data
+	//VBO
 	float boxVertices[] = {
 	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
 	 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
@@ -51,15 +50,24 @@ RenderObject::RenderObject() : shader(DEFAULT_SHADER){
 	-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
 	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 	};
-
 	glGenBuffers(1, &BoxVBO);
 	glBindBuffer(GL_ARRAY_BUFFER, BoxVBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(boxVertices), boxVertices, GL_STATIC_DRAW);
-
+	//AttribPointers
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(2);
+	//Textures
+	stbi_set_flip_vertically_on_load(true);
+	Texture2D texture1("Textures/T_UV_04.jpg");
+	Texture2D texture2("Textures/T_Noise_03a.png");
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture1.ID);
+	glActiveTexture(GL_TEXTURE1);
+	glBindTexture(GL_TEXTURE_2D, texture2.ID);
+	//Shader
+	//Already managed via the initializer list
 }
 
 void RenderObject::Render(glm::mat4 projection, glm::mat4 view){
