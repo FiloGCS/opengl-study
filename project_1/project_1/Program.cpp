c2a033fd509550ca4c5485e1c4918a3fc57d3e67
FiloGCS <guillermo.couceiro@gmail.com>
2024-07-23 01:40:02 +0200

    Added hacky camera controls

--- project_1/project_1/Program.cpp
+++ project_1/project_1/Program.cpp
@@ -15,17 +15,67 @@
 #include "Shader.h"
 #include "Texture2D.h"
 #include "Entity.h"
+#include "Camera.cpp"
 //stb_image library handles image loading
 //This needs to be last since we're #including stb_image.h in previous includes
 // and we can only implement it once!
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
-constexpr glm::vec3 CLEAR_COLOR = glm::vec3(0.15, 0.16, 0.13);
+//Global variables
+ImVec4 clear_color = ImVec4(0.15f, 0.16f, 0.13f, 1.00f);
+int window_width = 800;
+int window_height = 600;
+// Global camera object
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+// Variables for mouse handling
+float lastX = 400, lastY = 300;
+bool firstMouse = true;
+// Timing
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
 
+// Function prototypes
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);
 }
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn) {
+	float xpos = static_cast<float>(xposIn);
+	float ypos = static_cast<float>(yposIn);
+
+	if (firstMouse) {
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // Reversed since y-coordinates go from bottom to top
+
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
+	camera.ProcessMouseScroll(static_cast<float>(yoffset));
+}
+
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.ProcessKeyboard(LEFT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.ProcessKeyboard(RIGHT, deltaTime);
+}
+
 
 int main() {
 	//-----INIT GLFW
@@ -40,7 +90,7 @@ int main() {
 	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
 	//Create a window with GLFW
-	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Program", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(window_width, window_height, "OpenGL Program", NULL, NULL);
 	if (window == NULL) {
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
@@ -60,6 +110,9 @@ int main() {
 	//Setup Platform/Renderer backends
 	ImGui_ImplGlfw_InitForOpenGL(window, true); // Second param install_callback=true will install GLFW callbacks and chain to existing ones.
 	ImGui_ImplOpenGL3_Init();
+	//ImGui state variables
+    bool show_demo_window = false;
+
 
 
 	//-----INIT GLAD
@@ -72,9 +125,12 @@ int main() {
 
 	//We can register a callback function on the window that gets called each time the window is resized.
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	//Set the mouse and keyboard callback
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
 	 
 	//Create a viewport
-	glViewport(0, 0, 800, 600);
+	glViewport(0, 0, window_width, window_height);
 	//Enable depth Test
 	glEnable(GL_DEPTH_TEST);
 
@@ -88,9 +144,18 @@ int main() {
 	//Create a cube object
 	Entity myCube = Entity();
 
+
 	///RENDER LOOP
 	//The glfwWindowShouldClose function checks at the start of each loop iteration if GLFW has been instructed to close.
 	while (!glfwWindowShouldClose(window)){
+
+		// Per-frame time logic
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+		// Input
+		processInput(window);
+
 
 		//The glfwPollEvents function checks if any events are triggered
 		// (like keyboard input or mouse movement events),
@@ -102,36 +167,55 @@ int main() {
 		ImGui_ImplOpenGL3_NewFrame();
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
-		ImGui::ShowDemoWindow(); // Show demo window! :)
+		//If desired, show demo window
+		if (show_demo_window) {
+			ImGui::ShowDemoWindow(&show_demo_window);
+		}
+		//Create our own ImGui window
+#pragma region Test_ImGui_window
+		static float f = 0.0f;
+		static int counter = 0;
+		// Create a window called "Hello, world!" and append into it.
+		ImGui::Begin("Hello, world!");
+		// Display some text (you can use a format strings too)
+		ImGui::Text("This is some useful text.");
+		// Edit bools storing our window open/close state
+		ImGui::Checkbox("Demo Window", &show_demo_window);
+		// Edit 1 float using a slider from 0.0f to 1.0f
+		ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+		ImGui::SliderInt("Window Width", &window_width, 800, 1600);
+		ImGui::SliderInt("Window Height", &window_height, 450, 900);
+		glfwSetWindowSize(window, window_width, window_height);
+		// Edit 3 floats representing a color
+		ImGui::ColorEdit3("clear color", (float*)&clear_color);
+		ImGui::End();
+#pragma endregion
 
-		//GAME LOOP GOES HERE
-		//...
-		//...
 
-		glClearColor(CLEAR_COLOR.x, CLEAR_COLOR.y, CLEAR_COLOR.z, 1.0f);
+
+
+		//Clear Buffers
+		glClearColor(clear_color.x , clear_color.y, clear_color.z, clear_color.w);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		//MATRIX TRANSFORMATIONS
-		glm::mat4 view = glm::mat4(1.0f);
+		glm::mat4 view = camera.GetViewMatrix();
 		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 		glm::mat4 projection;
-		projection = glm::perspective(glm::radians(45.0f), 800.0f / 600, 0.1f, 100.0f);
+		projection = glm::perspective(glm::radians(45.0f), ((float)window_width/(float)window_height), 0.1f, 100.0f);
 
-		//Update and render Objects
+		//Update and render entities
 		for (Entity& entity : entities) {
 			entity.Update(glfwGetTime());
-			for (int i = 0; i < 1000; i++) {
-				entity.Render(projection, view); // Call the Render method on each Entity
-			}
+			entity.Render(projection, view); // Call the Render method on each Entity
 		}
-
 
 		//Render ImGui
 		ImGui::Render();
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
+		//Swap Buffers
 		glfwSwapBuffers(window);
-
 	}
 
 	//TERMINATION
